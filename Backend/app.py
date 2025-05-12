16b7935670ffd6ee97830181cc376211ae89f4ec
russellchao <russchaoman@gmail.com>
2025-05-12 19:19:11 -0400

    Enabled CORS

--- Backend/app.py
+++ Backend/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from main import get_weather
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/<location>")
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'; 
 import './App.css';
 
 function App() {
+
+  const [location, setLocation] = useState("");
+  const [condition, setCondition] = useState("");
+  const [searched, setSearched] = useState(false); 
+
+
+
+  const HandleSubmit = (event) => {
+    if (!searched) {
+        event.preventDefault(); // prevents page reload
+        setSearched(true); 
+    };
+  };
+
+
+
+  useEffect(() => {
+    if (location) {
+        const getCondition = async () => {
+            if (searched && location) {
+                const response = await fetch(`http://localhost:5000/${encodeURIComponent(location)}`)
+                setCondition(response); 
+            }
+        };
+        getCondition(); 
+    }
+
+  }, [searched, location]); 
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+      <h1>Get weather in your location</h1>
+
+      <input 
+        type="text" 
+        placeholder="Enter location..." 
+        style={{ marginLeft:"25px", height:"30px", width:"500px" }}
+        value={location}
+        onChange={(e) => [setLocation(e.target.value), setSearched(false)]}
         >
-          Learn React
-        </a>
-      </header>
+      </input>
+
+      <button 
+        type='submit' 
+        style={{ height:"37px", width:"100px", backgroundColor:"#7FFFD4" }}
+        onClick={HandleSubmit}
+        >
+        Submit
+      </button>
+
+      <p>Condition: {condition} </p>
+
     </div>
   );
 }
