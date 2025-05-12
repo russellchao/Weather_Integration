72d2b5a94c71381ecf5cd0f554ec3447ea38e0ef
russellchao <russchaoman@gmail.com>
2025-05-12 17:48:56 -0400

    main.py is properly fetching weather data

--- Backend/app.py
+++ Backend/app.py
@@ -1,5 +1,5 @@
-from weather_api_key import api_key
 from flask import Flask
+
 
 
 app = Flask(__name__)
@@ -11,8 +11,17 @@ def hello_world():
 
 
 @app.route("/weather/<location>")
-def get_location(): 
-    return "<p>Get location</p>"
+def get_location(location): 
+
+    response = requests.get(f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={location}")
+
+    if response.status_code == 200:
+        weather_data = response.json
+        print(weather_data)
+    else:
+        print("Error: unable to fetch weather data")
+
+    # return "<p>Get location</p>"
 
 
 
--- Backend/main.py
+++ Backend/main.py
@@ -1,12 +1,33 @@
+from weather_api_key import api_key
+import requests
+import json
 
 
 
+def get_weather(location):
 
+    response = requests.get(f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={location}")
+
+    if response.status_code == 200:
+        weather_data = response.json()
+        return weather_data  # You can customize this response format
+    else:
+        return {"error": "Unable to fetch weather data. Make sure your location is valid.\n"}, response.status_code
 
 
 
 if __name__ == "__main__": 
 
     while True:
-        location = input("Enter your location in the format: City, State: ")
-        print(location)
+        location = input("Enter your city, zip, or postcode: ")
+        
+        weather_data = get_weather(location)
+        # print(weather_data)
+        
+        city = weather_data['location']['name']
+        region = weather_data['location']['region']
+        country = weather_data['location']['country']
+        condition = weather_data['current']['condition']['text']
+
+        print(f"Location: {city}, {region}, {country}")
+        print(f"Weather conditions: {condition}\n")
